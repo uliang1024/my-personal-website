f5884a3c05885fd86c6a4c93dded844e1c4db36d
uliang <amisleo000@gmail.com>
2023-02-16 21:55:57 +0800

    contact 調整

--- website/static/js/index.js
+++ website/static/js/index.js
@@ -70,14 +70,14 @@ $.fn.timeline = function () {
         selectors.item.each(function (i) {
             min = $(this).offset().top;
             max = $(this).height() + $(this).offset().top;
-            if (i == itemLength - 2 && pos > min + $(this).height() / 2) {
+            if (i == itemLength - 2 && pos > min + $(this).height() / 2.5) {
                 selectors.item.removeClass(selectors.activeClass);
                 selectors.id.css(
                     "background-image",
                     "url(" + selectors.item.last().find(selectors.img).attr("src") + ")"
                 );
                 selectors.item.last().addClass(selectors.activeClass);
-            } else if (pos <= max - 40 && pos >= min) {
+            } else if (pos <= max - 40 && pos >= min - 100 ) {
                 selectors.id.css(
                     "background-image",
                     "url(" + $(this).find(selectors.img).attr("src") + ")"
