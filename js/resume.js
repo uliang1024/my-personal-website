a2226f0ea85b35c889d7fb5c9d729dc3027cce66
uliang <amisleo000@gmail.com>
2023-03-23 02:18:12 +0800

    側邊攔(初步)

---
+++ js/resume.js
@@ -0,0 +1,10 @@
+// const colorlibNavToggle = document.querySelector('.colorlib-nav-toggle');
+
+// colorlibNavToggle.addEventListener('click', () => {
+//     if (colorlibNavToggle.ariaExpanded == true) {
+//         colorlibNavToggle.classList.add('active');
+//     } else {
+//         colorlibNavToggle.classList.remove('active');
+//     }    
+// });
+
