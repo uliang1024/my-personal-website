49f5c70f871ed2feb690211a21d75a936145c631
uliang <amisleo000@gmail.com>
2023-03-23 09:09:38 +0800

    側邊攔完成

--- js/resume.js
+++ js/resume.js
@@ -1,10 +1,22 @@
-// const colorlibNavToggle = document.querySelector('.colorlib-nav-toggle');
+const colorlibAside = new bootstrap.Offcanvas('#colorlib-aside');
+const offcanvasBtn = document.querySelectorAll('.offcanvas-btn');
 
-// colorlibNavToggle.addEventListener('click', () => {
-//     if (colorlibNavToggle.ariaExpanded == true) {
-//         colorlibNavToggle.classList.add('active');
-//     } else {
-//         colorlibNavToggle.classList.remove('active');
-//     }    
-// });
+function myFunction(event) {
+    if (event.matches) {
+        colorlibAside.hide();
+        offcanvasBtn.forEach(element => {
+            element.style.display = 'block';
+        });
+    } else {
+        colorlibAside.show();
+        offcanvasBtn.forEach(element => {
+            element.style.display = 'none';
+        });
+    }
+}
 
+const x = window.matchMedia("(max-width: 1200px)")
+myFunction(x);
+x.addEventListener("change", function (e) {
+    myFunction(e);
+});
